29e6f5bd3bbcbeac4bebad04f78457da2664a597
Inception02 <127276038+Inception02@users.noreply.github.com>
2024-06-29 21:45:13 +0800

    Del (#2)
    * 添加删除，但未完成debug
    
    * 添加删除

--- client/main.go
+++ client/main.go
@@ -218,6 +218,39 @@ func downloadFile(masterIP, masterPort, fileName string, destPath string) error 
 	return nil
 }
 
+func deleteFile(masterIP, masterPort, fileName string) error {
+	resp, err := http.Get(fmt.Sprintf("http://%s:%s/delete?fileName=%s", masterIP, masterPort, fileName))
+	if err != nil {
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Error closing body")
+		}
+	}(resp.Body)
+
+	var deleteURLs map[string]string
+	if err := json.NewDecoder(resp.Body).Decode(&deleteURLs); err != nil {
+		return err
+	}
+
+	for _, url := range deleteURLs {
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				fmt.Println("Error closing body")
+			}
+		}(resp.Body)
+	}
+
+	return nil
+}
+
 func listFiles(masterIP, masterPort string) ([]string, error) {
 	resp, err := http.Get(fmt.Sprintf("http://%s:%s/list", masterIP, masterPort))
 	if err != nil {
@@ -253,7 +286,7 @@ func main() {
 			return
 		}
 		filePath := args[0]
-		err := uploadFile(config.Master.IP, config.Master.Port, filePath, 1024)
+		err := uploadFile(config.Master.IP, config.Master.Port, filePath, 1024*200)
 		if err != nil {
 			fmt.Println("Error uploading file:", err)
 		} else {
@@ -273,6 +306,20 @@ func main() {
 			fmt.Println("Error downloading file:", err)
 		} else {
 			fmt.Println("File downloaded successfully.")
+		}
+	}
+
+	commands["delete"] = func(args []string) {
+		if len(args) < 1 {
+			fmt.Println("Usage: delete <path>")
+			return
+		}
+		filePath := args[0]
+		err := deleteFile(config.Master.IP, config.Master.Port, filePath)
+		if err != nil {
+			fmt.Println("Error deleting file:", err)
+		} else {
+			fmt.Println("File deleted successfully.")
 		}
 	}
 
